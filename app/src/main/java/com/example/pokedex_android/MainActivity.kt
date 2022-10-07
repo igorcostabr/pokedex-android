195b8ac18c3f1052128fb6f75d94feec0424d873
iikonmyv <igorcostabr@gmail.com>
2022-10-07 15:49:48 -0300

    Finalmente deu certo o alinhamento

--- app/src/main/java/com/example/pokedex_android/MainActivity.kt
+++ app/src/main/java/com/example/pokedex_android/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.pokedex_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedex_android.domain.Pokemon
+import com.example.pokedex_android.domain.PokemonType
+import com.example.pokedex_android.view.PokemonAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvPokemons)
+
+        val charmander = Pokemon(
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+            4,
+            "Charmander",
+            listOf(
+                PokemonType("Fire")
+            )
+
+        )
+        val pokemons = listOf(charmander, charmander, charmander, charmander, charmander)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
